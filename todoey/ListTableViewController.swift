241de999f3392129ab77daafb5faf6fe75a9863d
Sebastian Malm <skmalm@gmail.com>
2020-06-18 18:49:29 -0400

    feat: new todos can be added to a list

--- todoey/ListTableViewController.swift
+++ todoey/ListTableViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
     
+    // MARK: - LIFECYCLE
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.backgroundColor = listColor
         navigationItem.title = list.name
     }
 
+    // MARK: - PROPERTIES
+    
     var list: TodoList!
     
     // white is used as default color
     var listColor = UIColor.white
+    
+    // MARK: - METHODS
+    
+    @IBAction func addTodo(_ sender: UIBarButtonItem) {
+        let newTodoAlert = UIAlertController(title: "Add New Todo", message: nil, preferredStyle: .alert)
+        newTodoAlert.addTextField { textField in
+            textField.placeholder = "Enter Todo Name"
+        }
+        newTodoAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak newTodoAlert, weak self] _ in
+            guard let self = self else { return }
+            let textField = newTodoAlert!.textFields![0]
+            if textField.text! == "" {
+                let emptyNameAlert = UIAlertController(title: "Error", message: "Todo name cannot be empty.", preferredStyle: .alert)
+                emptyNameAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(emptyNameAlert, animated: true, completion: nil)
+                return
+            } else if self.list.todos.contains(textField.text!) {
+                let duplicateNameAlert = UIAlertController(title: "Error", message: "There's already a todo with this name.", preferredStyle: .alert)
+                duplicateNameAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(duplicateNameAlert, animated: true, completion: nil)
+                return
+            } else {
+                self.tableView.performBatchUpdates({
+                    self.list.todos.append(textField.text!)
+                    self.tableView.insertRows(at: [IndexPath(row: self.list.todos.count - 1, section: 0)], with: .fade)
+                }, completion: { finished in
+                    if finished {
+                        self.tableView.scrollToRow(at: IndexPath(row: self.list.todos.count - 1, section: 0), at: .none, animated: true)
+                    }
+                })
+            }
+        }))
+        self.present(newTodoAlert, animated: true, completion: nil)
+    }
+    
     
     // MARK: - UITableViewDataSource
     
